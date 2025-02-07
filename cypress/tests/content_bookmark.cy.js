d6289e427ea658f0dba8cb3267e8367abe160984
Katja Süss <k.suess@rohberg.ch>
2025-02-07 19:32:03 +0100

    autologin earlier

--- cypress/tests/content_bookmark.cy.js
+++ cypress/tests/content_bookmark.cy.js
@@ -4,33 +4,34 @@ context('Bookmark Acceptance Tests', () => {
       cy.intercept('GET', `/**/*?expand*`).as('content');
       cy.intercept('GET', '/**/Document').as('schema');
       cy.intercept('GET', '/**/@bookmarks').as('bookmarks');
-      
-      cy.visit('/');
-      cy.viewport('macbook-16');
+      // Wait a bit to previous teardown to complete correctly because Heisenbug in this point
+      cy.wait(2000);
+      // give a logged in admin
+      cy.autologin();
+
       cy.createContent({
         contentType: 'Document',
         contentId: 'document',
         contentTitle: 'Test document',
       });
-      // give a logged in editor
-      cy.autologin();
-      cy.wait('@content')
-    });
 
+      cy.visit('/');
+      cy.wait('@content');
+    });
 
     it('As authenticated user I can bookmark a Page and see it in my bookmarks menu', function () {
       cy.visit('/document');
 
       // Bookmarks menu is empty
       cy.get('button#toolbar-show-bookmarks').click();
-      cy.wait('@bookmarks')
-      cy.contains("You don't have any bookmarks.")
+      cy.wait('@bookmarks');
+      cy.contains("You don't have any bookmarks.");
 
       cy.get('button#toolbar-addbookmark').click();
 
       // bookmarks menu contains new bookmark
       cy.get('button#toolbar-show-bookmarks').click();
-      cy.wait('@bookmarks')
+      cy.wait('@bookmarks');
       cy.get('.volto-bookmarks-list').contains('Test document');
     });
   });
