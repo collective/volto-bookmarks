410a1685c0506f0eb604b0872079132edf9ea629
Katja Süss <k.suess@rohberg.ch>
2025-02-08 20:18:58 +0100

    straighten up acceptance test

--- cypress/tests/content_bookmark.cy.js
+++ cypress/tests/content_bookmark.cy.js
@@ -14,23 +14,26 @@ context('Bookmark Acceptance Tests', () => {
         contentId: 'document',
         contentTitle: 'Test document',
       });
-
-      cy.visit('/');
-      cy.wait('@content');
     });
 
-    it('As authenticated user I can bookmark a Page and see it in my bookmarks menu', function () {
+    it('The bookmarks menu is initially empty', function () {
       cy.visit('/document');
 
       // Bookmarks menu is empty
       cy.get('button#toolbar-show-bookmarks').click();
       cy.wait('@bookmarks');
       cy.contains("You don't have any bookmarks.");
+    });
 
-      cy.get('button#toolbar-addbookmark').click();
+    it('As authenticated user I can bookmark a Page and see it in my bookmarks menu', function () {
+      cy.visit('/document');
 
-      // bookmarks menu contains new bookmark
-      cy.get('button#toolbar-show-bookmarks').click();
+      // Bookmark current page
+      cy.get('#toolbar-addbookmark').click();
+      cy.wait('@content');
+
+      // Bookmarks menu contains new bookmark
+      cy.get('#toolbar-show-bookmarks').click();
       cy.wait('@bookmarks');
       cy.get('.volto-bookmarks-list').contains('Test document');
     });
