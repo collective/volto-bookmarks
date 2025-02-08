d126b89d96c01e517e5fb26fa4fd035426da48dc
Katja Süss <k.suess@rohberg.ch>
2025-02-08 20:16:16 +0100

    Update example.cy.js

--- cypress/tests/example.cy.js
+++ cypress/tests/example.cy.js
@@ -1,19 +1,16 @@
 context('Example Acceptance Tests', () => {
   describe('Visit a page', () => {
     beforeEach(() => {
-      // Given a logged in editor
-      cy.viewport('macbook-16');
+      cy.autologin();
       cy.createContent({
         contentType: 'Document',
         contentId: 'document',
         contentTitle: 'Test document',
       });
-      cy.autologin();
     });
 
     it('As editor I can add edit a Page', function () {
-      cy.visit('/document');
-      cy.navigate('/document/edit');
+      cy.visit('/document/edit');
       cy.get('#toolbar-save').click();
     });
   });
